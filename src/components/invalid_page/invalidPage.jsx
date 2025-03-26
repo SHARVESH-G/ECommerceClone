f4ee024b08a63db36e17965ce47c45f2c98364d2
SHARVESH-G <sharveshg2405@gmail.com>
2025-03-26 23:27:58 +0530

    updated

--- src/components/invalid_page/invalidPage.jsx
+++ src/components/invalid_page/invalidPage.jsx
@@ -1,13 +1,22 @@
-import { Alert, Box } from "@mui/material";
+import { Alert, Box  ,Button,Typography} from "@mui/material";
 import React from "react";
 import invalidStyles from "./invalidPageStyles";
+import { useNavigate } from "react-router-dom";
 
 const InvalidPage = () => {
+  let navi = useNavigate()
+
+  function move(){
+    navi("/signup")
+  }
   return (
     <Box sx={invalidStyles.container}>
       <Alert variant="outlined" severity="error" sx={invalidStyles.alert}>
         404 | Page Not Found
       </Alert>
+      <Button onClick={move}>
+        <Typography>Login / Register here</Typography>
+      </Button>
     </Box>
   );
 };
--- src/components/invalid_page/invalidPageStyles.jsx
+++ src/components/invalid_page/invalidPageStyles.jsx
@@ -2,10 +2,13 @@ const invalidStyles = {
     container: {
       height: "100vh",
       display: "flex",
+      gap:'50px',
       justifyContent: "center",
       alignItems: "center",
+      flexDirection:'column'
     },
     alert: {
+      size:'12',
       width: "30%",
       fontSize: "25px",
       textAlign: "center",
